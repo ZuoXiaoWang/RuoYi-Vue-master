c431689dd116abcda2a61cbbd9c0537429c8bf82
admin <912996618@qq.com>
2023-04-13 10:18:10 +0800

    修改增加巡更任务点位bug

--- ruoyi-admin/src/main/java/com/ruoyi/app/controller/LoginController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/app/controller/LoginController.java
@@ -3,9 +3,15 @@ package com.ruoyi.app.controller;
 import com.ruoyi.common.constant.Constants;
 import com.ruoyi.common.core.controller.AppBaseController;
 import com.ruoyi.common.core.domain.AjaxResult;
+import com.ruoyi.common.core.domain.entity.SysPersonnel;
 import com.ruoyi.common.core.domain.model.AppLoginBody;
+import com.ruoyi.common.utils.StringUtils;
 import com.ruoyi.framework.app.service.AppLoginService;
+import com.ruoyi.system.service.ISysPersonnelService;
+import com.ruoyi.system.service.ISysPostService;
+import com.ruoyi.system.service.ISysUserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +21,13 @@ public class LoginController extends AppBaseController {
     @Autowired
     private AppLoginService appLoginService;
 
+    @Autowired
+    private ISysPersonnelService sysPersonnelService;
+
+    @Autowired
+    private ISysPostService postService;
+
+
 
     @PostMapping("/login")
     public AjaxResult login(@RequestBody AppLoginBody apploginBody) {
@@ -23,6 +36,22 @@ public class LoginController extends AppBaseController {
         // 生成令牌
         String token = appLoginService.appLogin(apploginBody.getPersonnelLoginName(), apploginBody.getPersonnelPassword());
         ajax.put(Constants.TOKEN, token);
+        return ajax;
+    }
+
+    /**
+     * 获取员工管理详细信息
+     */
+    @GetMapping("/info")
+    public AjaxResult getInfo() {
+        Long appUserId = getAppUserId();
+        AjaxResult ajax = AjaxResult.success();
+        ajax.put("posts", postService.selectPostAll());
+        if (StringUtils.isNotNull(appUserId)) {
+            SysPersonnel sysPersonnel = sysPersonnelService.selectSysPersonnelByPersonnelId(appUserId);
+            ajax.put(AjaxResult.DATA_TAG, sysPersonnel);
+            ajax.put("postIds", postService.selectPostListByPersonnelId(appUserId));
+        }
         return ajax;
     }
 
